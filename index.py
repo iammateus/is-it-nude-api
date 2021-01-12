44ed37f2716e62f7127228ab20c2d2c59dd03fc8
Mateus Soares <mateussoaress1997@gmail.com>
2021-01-12 19:49:17 -0300

    Refacoring docker-compose and index.py

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, request
-import nude
-from werkzeug.utils import secure_filename
 import os
+import nude
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/var/www/html/resources'
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -18,8 +19,7 @@ def isItNude():
     filename = secure_filename(imageFile.filename)
     filePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     imageFile.save(filePath)
-    data = {'isItNude': nude.is_nude(filePath)}
-    return jsonify(data)
+    return jsonify({"result": nude.is_nude(filePath)})
 
 # start the development server using the run() method
 if __name__ == "__main__":
